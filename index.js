09b49455214db0b5f4ede24420876462d5b90398
Hunt Collings <hunt@collings.co.uk>
2019-01-05 15:05:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ window.addEventListener("load", () => {
 function handleSubmit(e) {
   e.preventDefault();
   let passwordLength = parseInt(
-    document.querySelector("#passwordLength).value);
+    document.querySelector("#passwordLength").value);
 
   if (passwordLength < 4 || !passwordLength ) passwordLength = 4;
 
@@ -43,5 +43,5 @@ function generatePassword(length) {
 }
 
 function appendOutput(password) {
-  document.querySelector("output").innerText = password;
+  document.querySelector("#output").innerText = password;
 }
