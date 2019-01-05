58315ebd93ce38fd0be3300e6a08ddb6ff37b966
Hunt Collings <hunt@collings.co.uk>
2019-01-05 14:43:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 window.addEventListener("load", () => {
-  //start your code here
-});
+
+
+function generatePassword() {
+    
+    var length = document.getElementById("requestlength").value,
+        charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!#$%&()",
+        retVal = "";
+    for (var i = 0, n = charset.length; i < length; ++i) {
+        retVal += charset.charAt(Math.floor(Math.random() * n));
+    }
+    return retVal; }
+
+  
+}
+ );
+ 
+ generatePassword();
+ 
