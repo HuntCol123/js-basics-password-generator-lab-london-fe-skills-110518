a1af1050cd0794333185cd02e8bb75727de60f50
Hunt Collings <hunt@collings.co.uk>
2019-01-05 15:00:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ window.addEventListener("load", () => {
 function handleSubmit(e) {
   e.preventDefault();
   let passwordLength = parseInt(
-    document.getElementByID("passwordLength").value
+    document.getElementById(passwordLength).value
   );
 
   if (passwordLength < 4 || !passwordLength ) passwordLength = 4;
