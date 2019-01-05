43e3031f0c55171392646ff65c55dce2db7601e7
Hunt Collings <hunt@collings.co.uk>
2019-01-05 14:51:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,19 +1,49 @@
+const lowercaseLetters = "abcdefghijklmnopqrstuvwxyz".split("");
+const uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+const numbers = "0123456789".split("");
+const specialCharacters = "!#$%&()*+,-./:;<=>?@[\\]^_{|}~".split("");
+const allCharacters = lowercaseLetters
+  .concat(uppercaseLetters)
+  .concat(numbers)
+  .concat(specialCharacters);
+
 window.addEventListener("load", () => {
+  document.addEventListener('submit', handleSubmit)
+});
+
+function handleSubmit(e) {
+  e.preventDefault();
+  let passwordLength = parseInt(
+    document.querySelector("#passwordLength").value
+  );
+
+  if (passwordLength < 4 || !passwordLength ) passwordLength = 4;
 
 
-function generatePassword() {
-    
-    var length = document.getElementById("requestlength").value,
-        charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!#$%&()",
-        retVal = "";
-    for (var i = 0, n = charset.length; i < length; ++i) {
-        retVal += charset.charAt(Math.floor(Math.random() * n));
-    }
-    return retVal; }
 
-  
+  passwordLength
+    ? appendOutput(generatePassword(passwordLength))
+    : appendOutput("Please enter a valid password length");
 }
- );
- 
- generatePassword();
+
+function generatePassword(length) {
+  let password = "";
+  password +=
+    lowercaseLetters[Math.floor(Math.random() * lowercaseLetters.length)];
+  password +=
+    uppercaseLetters[Math.floor(Math.random() * uppercaseLetters.length)];
+  password += numbers[Math.floor(Math.random() * numbers.length)];
+  password +=
+    specialCharacters[Math.floor(Math.random() * specialCharacters.length)];
+
+  while (password.length < length) {
+    password += allCharacters[Math.floor(Math.random() * allCharacters.length)];
+  }
+
+  return password;
+}
+
+function appendOutput(password) {
+  document.querySelector("output").innerText = password;
+}
  
